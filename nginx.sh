8a11a478ff1ddcd07e5c52d37837504efe4d02ed
rcorales <47377687+rcorales@users.noreply.github.com>
2022-04-28 14:03:25 +0800

    Update nginx.sh

--- nginx.sh
+++ nginx.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 sudo apt update -y
 #sudo apt install nginx -y
-wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
-sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
-sudo apt update -y
-sudo apt install jenkins
-sudo ufw allow 8080
-sudo ufw allow OpenSSH
-sudo ufw enable
 
 
