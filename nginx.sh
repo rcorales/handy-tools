174a8bccbb0d7b5236e7020256883d71b43507ac
rcorales <47377687+rcorales@users.noreply.github.com>
2022-04-28 12:57:52 +0800

    Update nginx.sh

--- nginx.sh
+++ nginx.sh
@@ -8,6 +8,7 @@ wget https://apt.puppetlabs.com/puppet6-release-focal.deb
 sudo dpkg -i puppet6-release-focal.deb
 sudo apt-get update -y
 sudo apt-get install puppetserver -y
+sed -i 's/2/1/g' /etc/default/puppetserver
 #sudo nano /etc/default/puppetserver
 sudo systemctl start puppetserver
 sudo systemctl enable puppetserver
