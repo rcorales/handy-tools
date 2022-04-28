dfd153cec58bce1a4d98c08e053f7a0aa49de49c
rcorales <47377687+rcorales@users.noreply.github.com>
2022-04-28 12:52:31 +0800

    update puppet

--- nginx.sh
+++ nginx.sh
@@ -4,7 +4,11 @@ sudo apt install nginx -y
 wget https://apt.puppetlabs.com/puppet6-release-focal.deb
 sudo dpkg -i puppet6-release-focal.deb
 sudo apt-get update -y
-sudo apt-get install puppet-agent -y
-sudo systemctl start puppet
-sudo systemctl enable puppet
-#sudo systemctl status puppet
+wget https://apt.puppetlabs.com/puppet6-release-focal.deb
+sudo dpkg -i puppet6-release-focal.deb
+sudo apt-get update -y
+sudo apt-get install puppetserver -y
+#sudo nano /etc/default/puppetserver
+sudo systemctl start puppetserver
+sudo systemctl enable puppetserver
+
